4d69fe227df9f8a5d606b1d51f1460001e7ea99b
Vicky <mvictoriacejasbenjumea@gmail.com>
2026-04-10 19:57:49 +0200

    feat: create react project for sheet01

--- 03-react/README.md
+++
@@ -1,3 +0,0 @@
-# React
-
-En este bloque se trabajarán los ejercicios relacionados con React durante el bootcamp.
---
+++ 03-react/sheet01/src/components/exercise03/CorregirJSX.tsx
@@ -0,0 +1,12 @@
+function CorregirJSX() {
+    return (
+        <section className="hero">
+            <h1>React desde Cero</h1>
+            <p>Aprende a crear interfaces modernas</p>
+            <img src="https://via.placeholder.com/200" alt="Portada" />
+            <button>Empezar</button>
+        </section>
+    );
+}
+
+export default CorregirJSX;
