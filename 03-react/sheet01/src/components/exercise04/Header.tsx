022fcdfa6c0280c8fb63f04f27fbc342fa0cdfa0
Victoria <mvictoriacejasbenjumea@gmail.com>
2026-04-12 20:33:59 +0200

    refactor: move React project to 03-react root and reorganize folders

--- 03-react/sheet01/src/components/exercise04/Header.tsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <section>
-            <h1>Bloque de React - Bootcamp FullStack JAVA 9</h1>
-        </section>
-    );
-}
-
-export default Header;
---
+++ 03-react/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import WelcomeCard from './components/sheet01/exercise02/WelcomeCard.tsx';
+import CorregirJSX from './components/sheet01/exercise03/CorregirJSX.tsx';
+import Exercise04 from './components/sheet01/exercise04/Exercise04.tsx';
+
+function App() {
+    return (
+        <>
+            <h1>Hoja de ejercicios 01</h1>
+            <WelcomeCard />
+            <CorregirJSX />
+            <Exercise04 />
+        </>
+    );
+}
+
+export default App;
