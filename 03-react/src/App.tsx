d10d716d7d3d2c39127f8e78f8d61878bc3ed30b
Vicky <mvictoriacejasbenjumea@gmail.com>
2026-04-13 13:56:03 +0200

    feat: edit app documents react

--- 03-react/src/App.tsx
+++ 03-react/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
-// import WelcomeCard from './components/sheet01/exercise02/WelcomeCard.tsx';
-// import CorregirJSX from './components/sheet01/exercise03/CorregirJSX.tsx';
-// import Exercise04 from './components/sheet01/exercise04/Exercise04.tsx';
-import Exercise05 from './components/sheet01/exercise05/Exercise05.tsx';
+// import WelcomeCard from './components/sheet01/exercise02/WelcomeCard';
+// import CorregirJSX from './components/sheet01/exercise03/CorregirJSX';
+// import Exercise04 from './components/sheet01/exercise04/Exercise04';
+// import Exercise05 from './components/sheet01/exercise05/Exercise05';
+// import Exercise07 from './components/sheet01/exercise07/Exercise07';
+import Exercise03 from './components/sheet02/exercise03/Errors';
 
 function App() {
     return (
@@ -10,8 +12,10 @@ function App() {
             {/* <h1>Hoja de ejercicios 01</h1>
             <WelcomeCard />
             <CorregirJSX />
-            <Exercise04 /> */}
+            <Exercise04 /> 
             <Exercise05 />
+            <Exercise07 /> */}
+            <Exercise03 />
         </>
     );
 }
