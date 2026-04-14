ef5cd315c753a001075fca005df0b678dff78bd2
Victoria <mvictoriacejasbenjumea@gmail.com>
2026-04-14 20:06:10 +0200

    feat: add exercises 11 and 12 from react sheet03

--- 03-react/src/App.tsx
+++ 03-react/src/App.tsx
@@ -4,10 +4,15 @@ import './App.css';
 // import BasicComponentLayout from './components/sheet01/exercise04/Exercise04';
 // import ModulePresentation from './components/sheet01/exercise05/Exercise05';
 // import CourseLanding from './components/sheet01/exercise07/Exercise07';
-import ProfileCardExample from './components/sheet02/exercise03/Errors';
+// import ProfileCardExample from './components/sheet02/exercise03/Errors';
 // import BootcampModules from './components/sheet02/exercise05/Exercise05';
 // import PanelComposition from './components/sheet02/exercise07/Exercise07';
 // import CourseCatalog from './components/sheet02/exercise09/Exercise09';
+// import Sample from './components/sheet03/exercise03/Sample';
+// import MultipleValue from './components/sheet03/exercise05/ExperimentValue';
+// import FixSample from './components/sheet03/exercise08/FixSample';
+// import ChangeTheme from './components/sheet03/exercise11/ChangeTheme';
+import ItemCounter from './components/sheet03/exercise12/ItemCounter';
 
 function App() {
     return (
@@ -21,11 +26,18 @@ function App() {
             <CourseLanding /> 
             */}
 
-            <h1>Hoja de ejercicios 02</h1>
+            {/*<h1>Hoja de ejercicios 02</h1>
             <ProfileCardExample />
-            {/* <BootcampModules />
+            <BootcampModules />
             <PanelComposition />
             <CourseCatalog />  */}
+
+            <h1>Hoja de ejercicios 03</h1>
+            {/* <Sample /> 
+            <MultipleValue />
+            <FixSample /> 
+            <ChangeTheme /> */}
+            <ItemCounter />
         </>
     );
 }
--- 03-react/src/components/sheet03/exercise11/ChangeTheme.tsx
+++ 03-react/src/components/sheet03/exercise11/ChangeTheme.tsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
+import './Theme.css';
+
 function ChangeTheme() {
-    function handleTheme() {}
+    const [isDark, setIsDark] = useState(false);
+
+    function handleTheme() {
+        setIsDark(!isDark);
+    }
+
     return (
         <>
-            <div id="toggle">
+            <div id="toggle" className={isDark ? 'dark-theme' : 'white-theme'}>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nulla eaque omnis amet, corrupti nam
                     exercitationem consequatur harum alias accusamus tempore porro minima qui excepturi eum veniam
                     numquam quas aliquam.
                 </p>
             </div>
-            <button onClick={handleTheme}>Cambiar apariencia</button>
+            <button onClick={handleTheme}>{isDark ? 'Modo claro' : 'Modo oscuro'}</button>
         </>
     );
 }
