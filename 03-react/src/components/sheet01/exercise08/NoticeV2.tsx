95f328b568dc4d2ade3df21aca515551154dd5e7
Victoria <mvictoriacejasbenjumea@gmail.com>
2026-04-15 19:00:18 +0200

    feat: add exercises from react sheet 01

---
+++ 03-react/src/components/sheet01/exercise08/NoticeV2.tsx
@@ -0,0 +1,16 @@
+export default function NoticeV2() {
+    return (
+        <>
+            <h1>Esta es la V2, usando un fragmento</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus similique doloremque dignissimos maiores
+                officiis ducimus perspiciatis sapiente, quis modi magni nobis sequi deserunt reiciendis a in hic
+                repellendus placeat aut?
+            </p>
+            <ul>
+                <li>Elemento de lista 1</li>
+                <li>Elemento de lista 2</li>
+            </ul>
+        </>
+    );
+}
