27d32298287e299a1a9b1f347e5336f2dcccf231
Victoria <mvictoriacejasbenjumea@gmail.com>
2026-04-21 19:32:41 +0200

    feat: add poke filter

--- frontend/mundo-pokemon-react/src/App.tsx
+++ frontend/mundo-pokemon-react/src/App.tsx
@@ -1,11 +1,15 @@
 import './styles/style.css';
 import './styles/types.css';
+import { useState } from 'react';
 import PokemonList from './components/PokemonList';
+import PokeSearch from './components/PokemonSearch';
 
 export default function App() {
+    const [searchPokemon, setSearchPokemon] = useState('');
     return (
         <main className="container">
-            <PokemonList />
+            <PokeSearch search={searchPokemon} handleChange={setSearchPokemon} />
+            <PokemonList search={searchPokemon} />
         </main>
     );
 }
