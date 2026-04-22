521433d852a1a76d2dbd13da02c77bbab2e0c7cb
Vicky <mvictoriacejasbenjumea@gmail.com>
2026-04-22 10:25:50 +0200

    feat: optimize PokemonFetch (not finished yet)

--- frontend/mundo-pokemon-react/src/components/utils/FetchPokemon.ts
+++ frontend/mundo-pokemon-react/src/components/utils/FetchPokemon.ts
@@ -1,9 +1,22 @@
-export async function fetchPokemons() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=-1');
+import type { Pokemon, PokemonBasic } from './Pokemon';
+
+const capitalize = (name: string): string => name.charAt(0).toUpperCase() + name.slice(1);
+
+const getBestImage = (sprites: any): string => {
+    return (
+        sprites.versions?.['generation-v']?.['black-white']?.animated?.front_default ??
+        sprites.other?.['official-artwork']?.front_default ??
+        sprites.front_default ??
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'
+    );
+};
+
+export async function fetchPokemons(limit: number = 151): Promise<Pokemon[]> {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
     const data = await response.json();
 
-    const pokemonsData = await Promise.all(
-        data.results.map(async (pokemon) => {
+    return Promise.all(
+        data.results.map(async (pokemon: PokemonBasic) => {
             const pokeRes = await fetch(pokemon.url);
             const pokeData = await pokeRes.json();
 
@@ -12,17 +25,11 @@ export async function fetchPokemons() {
 
             return {
                 id: pokeData.id,
-                nombre: pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1),
-
-                tipo: pokeData.types.map((type) => type.type.name),
-
-                img:
-                    pokeData.sprites.versions['generation-v']['black-white'].animated.front_default ??
-                    pokeData.sprites.front_default,
-
-                evolucion: speciesData.evolves_from_species ? speciesData.evolves_from_species.name : null,
+                nombre: capitalize(pokeData.name),
+                tipo: pokeData.types.map((type: any) => type.type.name),
+                img: getBestImage(pokeData.sprites),
+                evolucion: speciesData.evolves_from_species ? capitalize(speciesData.evolves_from_species.name) : null,
             };
         }),
     );
-    return pokemonsData;
 }
