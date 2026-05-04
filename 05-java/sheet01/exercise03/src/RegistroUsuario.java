3c6cd1b0b90068ecfd83302ac32de0fc3c5cad0e
Victoria <mvictoriacejasbenjumea@gmail.com>
2026-05-04 21:18:05 +0200

    feat: finish java exercise03

--- 05-java/sheet01/exercise03/src/RegistroUsuario.java
+++ 05-java/sheet01/exercise03/src/RegistroUsuario.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class RegistroUsuario {
     public static void main(String[] args) {
-        String nombre;
-        String email;
-        int edad;
-    }
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Introduce tu edad: ");
+        int edad = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.print("Introduce tu email: ");
+        String email = sc.nextLine();
+
+
+        System.out.print("Usuario registrado: \n Nombre: " + nombre + "\n Edad: " + edad + "\n Email: " + email);
+    }
 }
